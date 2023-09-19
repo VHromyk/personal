2d60312a93db217d53cbe76343e46281167f5aa5
VHromyk <65778221+VHromyk@users.noreply.github.com>
2023-09-19 17:17:21 +0300

    feat: addded openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,13 @@ export const metadata: Metadata = {
   title: 'Vitalii Hromyk | Resume',
   description:
     'Detailed resume of Vitalii Hromyk, a skilled frontend developer with experience in modern web technologies and a passion for creating intuitive user interfaces.',
+  openGraph: {
+    title: 'Vitalii Hromyk | Resume',
+    description: 'Detailed resume of Vitalii Hromyk, a skilled frontend developer with experience in modern web technologies and a passion for creating intuitive user interfaces.',
+    type: 'website',
+    url: 'www.vitalii-hromyk.com',
+    images: '/openGraph.jpg',
+  },
 }
 
 export default function RootLayout({
